ff7246de2fa98d87731830b5c7a1dc64350c16aa
DanielZhu <enterzhu@gmail.com>
2016-06-09 23:56:45 +0800

    Define dianping's config for shopList and shopComment Define the model

--- routes/spider.js
+++ routes/spider.js
@@ -2,13 +2,20 @@
 var express = require('express');
 var router = express.Router();
 
-var dp = require('../lib/dp');
+var dp = require('../lib/dpSpider');
+// var nuomi = require('../lib/nuomi');
 
 /* GET users listing. */
 router.get('/dp', function (req, res, next) {
   dp();
   res.send('respond with a resource');
 });
+
+/* GET users listing. */
+// router.get('/nuomi', function (req, res, next) {
+//   nuomi();
+//   res.send('respond with a resource');
+// });
 
 // router.post('/', function (req, res) {
 //   res.send('Got a POST request');
